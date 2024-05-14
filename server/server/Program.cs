0d71ef5d2ce8fee68ee9d8337da7378c080a4438
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2024-05-14 13:50:47 +0200

    richiesta funzionante per embedding

--- server/server/Program.cs
+++ server/server/Program.cs
@@ -6,12 +6,12 @@ using client.Model;
 namespace client;
 
 public class Program
-{     
+{
     static HttpClient client = new HttpClient();
     public static void Main(string[] args)
     {
-        string resourceName = "your-resource-name"; //inserire il proprio resource name
-        string apiKey = "your-api-key"; //inserire la propria api-key
+        string resourceName = ""; //inserire il proprio resource name
+        string apiKey = ""; //inserire la propria api-key
         string deploymentId = "ada-embedding"; // inserire il modello del deploy
         string apiVersion = "2024-03-01-preview"; // inserire la versione delle api
         string urlEmbedding = $"https://{resourceName}.openai.azure.com/openai/deployments/{deploymentId}/embeddings?api-version={apiVersion}";
@@ -40,13 +40,17 @@ public class Program
 
         app.MapGet("/embedding", async (HttpContext httpContext) =>
         {
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("api-key", apiKey);
-
+            // Aggiungi l'intestazione api-key
+            client.DefaultRequestHeaders.Add("api-key", apiKey);
             var requestBody = new
             {
                 input = new[] { "Let me cook" } //poi da inserire i chunk
             };
-
+            //var requestBody = new
+            //{
+            //    input = new[] { "iphone", "apple" }, //poi da aggiungere i chunk
+            //    dimensions = 5
+            //};
             var json = JsonSerializer.Serialize(requestBody);
             var content = new StringContent(json, Encoding.UTF8, "application/json");
 
@@ -54,15 +58,14 @@ public class Program
 
             if (response.IsSuccessStatusCode)
             {
-                var responseContent = await response.Content.ReadAsStreamAsync();
-                var resultJson = JsonSerializer.Serialize(responseContent, new JsonSerializerOptions { WriteIndented = true });
+                var responseContent = await response.Content.ReadAsStringAsync();
+                var resultJson = JsonSerializer.Serialize(JsonDocument.Parse(responseContent).RootElement, new JsonSerializerOptions { WriteIndented = true });
                 return Results.Content(resultJson, "application/json");
             }
             else
             {
                 return Results.StatusCode((int)response.StatusCode);
             }
-
 
         }).WithName("Embedding");
 
