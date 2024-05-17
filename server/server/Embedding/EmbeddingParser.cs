9d92fd91421656a2ba1dda6dac6ed238da9e675d
RedBoy <arosioalex06@gmail.com>
2024-05-17 13:23:39 +0200

    fix: Embedding interface for chunking

--- server/server/Embedding/EmbeddingParser.cs
+++ server/server/Embedding/EmbeddingParser.cs
@@ -6,18 +6,7 @@ namespace client.Embedding;
 public abstract class EmbeddingParser : IService
 {
     private IService _serviceImplementation;
-    public abstract Task<float[]> GetChunkEmbeddingAsync(DocumentChunk chunk);
-
-
-    public async Task<float[][]> GetChunksEmbeddingAsync(DocumentChunk[] chunks)
-    {
-        List<Task<float[]>> embeddingChunk = new();
-        chunks.ToList()
-            .ForEach(chunk => embeddingChunk.Add(GetChunkEmbeddingAsync(chunk)));
-        
-        return await Task.WhenAll(embeddingChunk);
-    }
-
+    public abstract Task<float[][]> GetChunkEmbeddingAsync(DocumentChunk[] chunk);
     public abstract Task<DocumentChunk> GetContextChunk(Message message);
 
     public abstract void PreLoad(WebApplicationBuilder builder);
