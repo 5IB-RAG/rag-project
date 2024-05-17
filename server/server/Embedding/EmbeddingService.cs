346e1cc69e0736bf644ad3be7185edfa4b8aa1b0
GiorgioCitterio <giorgio.citterio@issgreppi.it>
2024-05-17 13:05:28 +0200

    feat: fatto qualcosa test

---
+++ server/server/Embedding/EmbeddingService.cs
@@ -0,0 +1,17 @@
+﻿using client.Model;
+using System.Text.Json;
+using System.Text;
+
+namespace client.Embedding
+{
+    //public class EmbeddingService : IEmbedding
+    //{
+    //    private static HttpClient client = new HttpClient();
+    //    public async Task<EmbeddingResponse> GetEmbedding(List<string> parsedDocument)
+    //    {
+           
+
+            
+    //    }
+    //}
+}
---
+++ server/server/Embedding/IEmbedding.cs
@@ -0,0 +1,9 @@
+﻿using client.Model;
+
+namespace client.Embedding
+{
+    public interface IEmbedding
+    {
+        Task<EmbeddingResponse> GetEmbedding();
+    }
+}
