13546385f3a6580b860ae8235cdbecd941880eab
MarcoPassoni <marco.passoni@issgreppi.it>
2024-05-18 16:05:52 +0200

    update inserimento dello script per embedding

--- Mantenimento_Contesto/Mantenimento_Contesto/Program.cs
+++ Mantenimento_Contesto/Mantenimento_Contesto/Program.cs
@@ -1,30 +1,32 @@
 ﻿using Dapper;
 using MySqlConnector;
+using Org.BouncyCastle.Asn1.Mozilla;
 using Renci.SshNet;
 using System.Data;
+using System.Text.Json;
+using System.Text;
 
 namespace Mantenimento_Contesto
 {
     internal class Program
     {
-        static async Task Main(string[] args)
+        #region variabili
+        public string sshHost = "158.180.235.79";
+        public string database = "embeddingdb";
+        public string username = "embedding";
+        public string password = "Embedding2024@";
+        public string sshUsername = "ubuntu";
+        public string sshKeyFilePath = "../../../ssh-key-2024-01-26.key";
+        public int sshPort = 22;
+        public string dbHost = "localhost";
+        public int dbPort = 3306;
+        public int localPort = 3307;
+        public string azureApiKey = "5619cf938d0241c2a7bc08eb7a692a83";
+        public string azureEndpoint = "https://Passoni-Embedding.openai.azure.com/openai/deployments/ada-embedding/embeddings?api-version=2023-03-15-preview";
+        #endregion
+
+        public async Task InterfacciaDb()
         {
-            //indirizzo ip: 158
-            //password db: Embedding2024@
-            //utente db: embedding
-            string sshHost = "158.180.235.79";
-            string database = "embeddingdb";
-            string username = "embedding";
-            string password = "Embedding2024@";
-
-            string sshUsername = "ubuntu";
-            string sshKeyFilePath = "../../../ssh-key-2024-01-26.key";
-            int sshPort = 22;
-
-            string dbHost = "localhost";
-            int dbPort = 3306;
-            int localPort = 3307;
-
             var privateKeyFile = new PrivateKeyFile(sshKeyFilePath);
             var authenticationMethod = new PrivateKeyAuthenticationMethod(sshUsername, privateKeyFile);
             var connectionInfo = new ConnectionInfo(sshHost, sshPort, sshUsername, authenticationMethod);
@@ -43,16 +45,48 @@ namespace Mantenimento_Contesto
                     Console.WriteLine("tentativo di connessione...");
                     connection.Open();
                     Console.WriteLine("tentativo riuscito!");
-                    string query = "SELECT testoinchiaro FROM chiaro"; // Sostituisci con una tua query
+                    string query = "INSERT testoinchiaro FROM chiaro"; // Sostituisci con una tua query
                     var count = await connection.QueryAsync<string>(query);
 
                     Console.WriteLine("il numero di righe sono: " + count.ToList()[0]);
                     // Esegui le operazioni sul database
                 }
-
                 portForwarded.Stop();
                 client.Disconnect();
             }
+        }
+
+        public async Task RichiestaEmbedding(string testText)
+        {
+            if (testText is null || testText == "")
+            {
+                return;
+            }
+            Console.WriteLine("richiesta partita");
+            // Invia la richiesta e ottieni la risposta
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("api-key", azureApiKey);
+                var inputContent = new { input = testText };
+                var json = JsonSerializer.Serialize(inputContent);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                Console.WriteLine(content.Headers);
+                var response = await client.PostAsync(azureEndpoint, content);
+                Console.WriteLine(response);
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.WriteLine(await response.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    // Handle the error
+                }
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            
         }
     }
 }
