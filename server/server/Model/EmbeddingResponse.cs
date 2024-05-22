04cbddd3b044b4e39bc1e1ecb0f12f89bb0808e3
EURedBoy <arosioalex06@gmail.com>
2024-05-22 18:34:05 +0200

    feat: temporary api test

---
+++ server/server/Model/EmbeddingResponse.cs
@@ -0,0 +1,24 @@
+﻿using Pgvector;
+
+namespace client.Model;
+
+public class EmbeddingResponse
+{
+    public string Object { get; set; }
+    public List<EmbeddingData> Data { get; set; }
+    public string Model { get; set; }
+    public EmbeddingUsage Usage { get; set; }
+}
+
+public class EmbeddingData
+{
+    public string Object { get; set; }
+    public int Index { get; set; }
+    public float[] Embedding { get; set; }
+}
+
+public class EmbeddingUsage
+{
+    public int PromptTokens { get; set; }
+    public int TotalTokens { get; set; }
+}
