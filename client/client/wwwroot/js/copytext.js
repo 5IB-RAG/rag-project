76e74c9e3cdb554ae22582e45514bae8cdef39c5
DavForme <davide.formenti@issgreppi.it>
2024-06-06 09:48:00 +0200

    feat: popup belli quando si copia il testo

--- client/client/wwwroot/js/copytext.js
+++ client/client/wwwroot/js/copytext.js
@@ -3,8 +3,21 @@
     console.log(copyText)
 
     navigator.clipboard.writeText(copyText.textContent).then(function () {
-        alert("Testo copiato negli appunti: " + copyText.textContent)
+        Swal.fire({
+            icon: 'sucess',
+            title: 'Testo copiato',
+            background: '#222',
+            color: '#fff',
+            showConfirmButton: false,
+            timer: 1500
+        });
     }).catch(function (error) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Errore nel copiare il testo',
+            showConfirmButton: false,
+            timer: 1500
+        });
         console.error("Errore nel copiare il testo: ", error);
     });
 }
